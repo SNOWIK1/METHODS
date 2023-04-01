7579f20e07a3222e9b693026fdcf46e0dedc0242
SNOWIK1 <dimais444@mail.ru>
2023-04-01 11:23:50 +0700

    bashed from comp-02

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static double Tg(double x) => Math.Sin(x) / Math.Cos(x);
+
+        static double Rad(double x)
+
+        {
+            return ((Math.PI * x) / 180);
+        }
+
+        static int Len(string x)
+        {
+            string[] splitted = x.Split('.');
+            string result = String.Join(' ', splitted);
+            string res = result.Replace(" ", "");
+            return res.Length;
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a float number to calculate: c = y * tg(y) + tg(y), where y - entered float number. ");
+            string enter = Console.ReadLine();
+            double num = Double.Parse(enter);
+
+            Console.WriteLine("c = " + ( num * Tg(num) + Tg(num)) );
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            Console.WriteLine("\nEnter a degrees value to convert it in radians");
+            string degrees = Console.ReadLine();
+            double deg = Double.Parse(degrees);
+
+            Console.WriteLine($"It will be {Rad(deg)} radians");
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            Console.WriteLine("\nEnter a number to learn about quantity of numbers in it: ");
+
+            string number = Console.ReadLine();
+            Console.WriteLine($"Amount of numbers: {Len(number)}");
+
+        }
+
+
+
+    }
+}
